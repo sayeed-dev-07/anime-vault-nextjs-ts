5d1c4eee2b0160ef6f4ec87fa2bb0a3bdd7de7e2
sayeed <expsayeedshorif@gmail.com>
2025-12-11 12:35:14 +0600

    also changed the anime api split problem

--- app/(anime)/animes/[id]/page.tsx
+++ app/(anime)/animes/[id]/page.tsx
@@ -4,8 +4,8 @@ import CardDetails from '@/components/CardDetails';
 
 const page = async({ params }: { params: Promise<{ id: string }>}) => {
     const {id} = await params;
-    const [name, ...genId] = id.split('-')
-    const res = await fetch(`https://api.jikan.moe/v4/anime/${genId}`)
+    const mangaId = id.split('-').pop();
+    const res = await fetch(`https://api.jikan.moe/v4/anime/${mangaId}`)
     const resData = await res.json();
     const animeData = resData.data;
     return (
